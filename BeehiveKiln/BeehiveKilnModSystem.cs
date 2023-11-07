8aeac2044fd67cb668c7b5b6fabcc073f8b1cc9f
Zach2039 <sumyunguy2007@gmail.com>
2023-11-06 22:23:14 -0600

    Migrate to net7

---
+++ BeehiveKiln/BeehiveKilnModSystem.cs
@@ -0,0 +1,19 @@
+﻿using beehivekiln.block;
+using beehivekiln.blockentity;
+using Vintagestory.API.Common;
+
+namespace beehivekiln
+{
+    class BeehiveKilnCore : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+
+            api.Logger.Notification("Loaded Beehive Kiln!");
+
+            api.RegisterBlockClass("BlockFirebrickKilnFlue", typeof(BlockFirebrickKilnFlue));
+            api.RegisterBlockEntityClass("BlockEntityBeehiveKiln", typeof(BlockEntityBeehiveKiln));
+        }
+    }
+}
---
+++ BeehiveKiln/block/BlockFirebrickKilnFlue.cs
@@ -0,0 +1,60 @@
+﻿using beehivekiln.blockentity;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace beehivekiln.block
+{
+	public class BlockFirebrickKilnFlue : Block
+	{
+		public bool ControllerBlock
+		{
+			get
+			{
+				return this.EntityClass != null;
+			}
+		}
+
+
+		public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
+		{
+			BlockPos pos = blockSel.Position;
+			if (pos == null)
+			{
+				return base.OnBlockInteractStart(world, byPlayer, blockSel);
+			}
+			BlockEntityBeehiveKiln bebk = world.BlockAccessor.GetBlockEntity(pos) as BlockEntityBeehiveKiln;
+			if (bebk != null)
+			{
+				bebk.Interact(byPlayer, !this.ControllerBlock);
+				IClientPlayer clientPlayer = byPlayer as IClientPlayer;
+				if (clientPlayer != null)
+				{
+					clientPlayer.TriggerFpAnimation(EnumHandInteract.HeldItemInteract);
+				}
+				return true;
+			}
+			return base.OnBlockInteractStart(world, byPlayer, blockSel);
+		}
+
+		public int GetTemperature(IWorldAccessor world, BlockPos pos)
+		{
+			if (pos == null)
+			{
+				return 0;
+			}
+			BlockEntityBeehiveKiln besc = world.BlockAccessor.GetBlockEntity(pos) as BlockEntityBeehiveKiln;
+			if (besc != null)
+			{
+				return besc.KilnTemperature;
+			}
+			return 0;
+		}
+
+		public override void OnBlockBroken(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1f)
+		{
+			base.OnBlockBroken(world, pos, byPlayer, dropQuantityMultiplier);
+			world.BlockAccessor.RemoveBlockEntity(pos);
+		}
+	}
+}
